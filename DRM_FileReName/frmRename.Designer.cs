7128867ad2e6833e1b6c5126ee4b3f63f9fcf0b2
Jayman Gandhi <jayman.gandhi@outlook.com>
2018-04-15 10:40:58 -0400

    First commit

---
+++ DRM_FileReName/frmRename.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DRM_FileReMame
+{
+    partial class frmRename
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSourceFolder = new System.Windows.Forms.TextBox();
+            this.btn_Explorer = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Folder:";
+            // 
+            // txtSourceFolder
+            // 
+            this.txtSourceFolder.Location = new System.Drawing.Point(122, 51);
+            this.txtSourceFolder.Name = "txtSourceFolder";
+            this.txtSourceFolder.Size = new System.Drawing.Size(244, 20);
+            this.txtSourceFolder.TabIndex = 1;
+            // 
+            // btn_Explorer
+            // 
+            this.btn_Explorer.Location = new System.Drawing.Point(397, 51);
+            this.btn_Explorer.Name = "btn_Explorer";
+            this.btn_Explorer.Size = new System.Drawing.Size(32, 23);
+            this.btn_Explorer.TabIndex = 2;
+            this.btn_Explorer.Text = "...";
+            this.btn_Explorer.UseVisualStyleBackColor = true;
+            this.btn_Explorer.Click += new System.EventHandler(this.btn_Explorer_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(122, 103);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "&Start";
+            this.btnStart.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(291, 102);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmRename
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 173);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btn_Explorer);
+            this.Controls.Add(this.txtSourceFolder);
+            this.Controls.Add(this.label1);
+            this.Name = "frmRename";
+            this.Text = "Rename Files";
+            this.Load += new System.EventHandler(this.frmRename_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSourceFolder;
+        private System.Windows.Forms.Button btn_Explorer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ DRM_FileReName/frmRename.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DRM_FileReMame
+{
+    public partial class frmRename : Form
+    {
+        public frmRename()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btn_Explorer_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                //
+                // The user selected a folder and pressed the OK button.
+                // We print the number of files found.
+                //
+                txtSourceFolder.Text = folderBrowserDialog1.SelectedPath;
+                if (txtSourceFolder.Text.Trim() == null)
+                {
+                    btnStart.Enabled = false;
+                }
+                else
+                {
+                    btnStart.Enabled = true;
+                }
+            }
+        }
+
+        private void frmRename_Load(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+        }
+
+        private int StartConversion_FileNameWithSemiColon (string sFilePath)
+        {
+            int iFileCount = 0;
+            int iSeperatorPos = 0;
+            int iDashSpaceCount = 0;
+
+            string[] files = Directory.GetFiles(sFilePath);
+
+            foreach (string sFile in files)
+            {
+                iSeperatorPos = sFile.IndexOf(';');
+
+                if (iSeperatorPos > 0)
+                {
+                    iDashSpaceCount = sFile.Split('-').Length;
+
+                    if (iDashSpaceCount == 3)
+                    {
+                        var sPartName = sFile.Substring(iSeperatorPos + 1, 8);
+                        DateTime dDate = System.Convert.ToDateTime(sPartName);
+
+                        MessageBox.Show(dDate.ToString());
+                    }
+                    else if (iDashSpaceCount == 4)
+                    {
+                        var sPartName = sFile.Substring(iSeperatorPos + 1, 8);
+                        DateTime dDate = System.Convert.ToDateTime(sPartName);
+
+                        MessageBox.Show(dDate.ToString());
+
+                    }
+                }
+
+                iSeperatorPos = sFile.IndexOf('_');
+
+                if (iSeperatorPos > 0)
+                {
+                    iDashSpaceCount = sFile.Split('-').Length;
+
+                    if (iDashSpaceCount == 1)
+                    {
+                        var sPartName = sFile.Substring(iSeperatorPos + 1, 8);
+                        DateTime dDate = System.Convert.ToDateTime(sPartName);
+
+                        MessageBox.Show(dDate.ToString());
+                    }
+                    else if (iDashSpaceCount == 2)
+                    {
+                        var sPartName = sFile.Substring(iSeperatorPos + 1, 8);
+                        DateTime dDate = System.Convert.ToDateTime(sPartName);
+
+                        MessageBox.Show(dDate.ToString());
+
+                    }
+                    else if (iDashSpaceCount == 3)
+                    {
+                        var sPartName = sFile.Substring(iSeperatorPos + 1, 8);
+                        DateTime dDate = System.Convert.ToDateTime(sPartName);
+
+                        MessageBox.Show(dDate.ToString());
+
+                    }
+                    else
+                    {
+                        var sPartName = sFile.Substring(iSeperatorPos + 1);
+                        DateTime dDate = System.Convert.ToDateTime(sPartName);
+
+                        MessageBox.Show(dDate.ToString());
+                    }
+                }
+
+            }
+
+
+            return iFileCount;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartConversion_FileNameWithSemiColon(txtSourceFolder.Text.Trim());
+        }
+    }
+}
